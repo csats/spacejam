12e6b890d01a6f146d978cd90b6b381e75036e94
Michiel ter Reehorst <michiel@swydo.com>
2018-05-08 12:23:15 +0200

    Remove any phantomjs referral

--- tests/lib/CLITest.js
+++ tests/lib/CLITest.js
@@ -8,7 +8,6 @@ const DEFAULT_PATH = process.env.PATH;
 
 const fs = require('fs');
 const path = require('path');
-const phantomjs = require("phantomjs-prebuilt");
 const chai = require("chai");
 const { expect } = chai;
 const sinon = require("sinon");
@@ -31,7 +30,6 @@ describe("CLI", function() {
   let exitStub = null;
   let testPackagesStub = null;
   let spawnSpy = null;
-  let phantomjsScript = null;
 
   before(() => processArgv = process.argv);
 
@@ -51,7 +49,6 @@ describe("CLI", function() {
     ({ spacejam } = cli);
     exitStub = sinon.stub(process, 'exit');
     testPackagesStub = sinon.stub(spacejam, 'runTests');
-    return phantomjsScript = 'phantomjs-test-in-console.js';
   });
 
   afterEach(function() {
@@ -80,61 +77,6 @@ describe("CLI", function() {
     process.argv.push('test-packages', '--settings', 'settings.json', 'package1', 'package2');
     cli.exec();
     return expect(testPackagesStub).to.have.been.calledWith("test-packages", {command: "test-packages", settings: 'settings.json', packages: ['package1', 'package2']});
-  });
-
-  it("should spawn phantomjs with the value of --phantomjs-options", function(done){
-    log.setLevel('debug');
-    testPackagesStub.restore();
-    spawnSpy = sinon.spy(ChildProcess, '_spawn');
-    process.chdir(__dirname + "/../apps/leaderboard/packages/success");
-    // We set mongo-url to mongodb:// so test will be faster
-    process.argv.push('test-packages', '--port', '11096', '--mongo-url', 'mongodb://', '--phantomjs-options=--ignore-ssl-errors=true --load-images=false', './');
-    cli.exec();
-    return spacejam.on('done', code=> {
-      try {
-        if (code === 0) { done(); } else { done(`spacejam.done=${code}`); }
-        expect(spawnSpy).to.have.been.calledTwice;
-        return expect(spawnSpy.secondCall.args[1]).to.deep.equal(['--ignore-ssl-errors=true', '--load-images=false', phantomjsScript]);
-      } catch (err) {
-        return done(err);
-      }
-    });
-  });
-
-  it("should modify PATH to include the path to the bundled phantomjs", function(done){
-    testPackagesStub.restore();
-    process.chdir(__dirname + "/../apps/leaderboard/packages/success");
-    // We set mongo-url to mongodb:// so test will be faster
-    process.argv.push('test-packages', '--port', '12096', '--mongo-url', 'mongodb://', '--phantomjs-options=--ignore-ssl-errors=true --load-images=false', './');
-    cli.exec();
-    return spacejam.on('done', code=> {
-      try {
-        if (code === 0) { done(); } else { done(`spacejam.done=${code}`); }
-
-        const firstPathEntry = process.env.PATH.split(":")[0];
-        return expect(firstPathEntry).to.equal(path.dirname(phantomjs.path));
-      } catch (err) {
-        return done(err);
-      }
-    });
-  });
-
-  return it("should not modify PATH if --use-system-phantomjs is given", function(done){
-    testPackagesStub.restore();
-    process.chdir(__dirname + "/../apps/leaderboard/packages/success");
-    // We set mongo-url to mongodb:// so test will be faster
-    process.argv.push('test-packages', '--port', '13096', '--mongo-url', 'mongodb://', '--use-system-phantomjs', '--phantomjs-options=--ignore-ssl-errors=true --load-images=false', './');
-    console.log(process.argv.join(" "));
-    cli.exec();
-    return spacejam.on('done', code=> {
-      try {
-        if (code === 0) { done(); } else { done(`spacejam.done=${code}`); }
-
-        return expect(process.env.PATH).to.equal(DEFAULT_PATH);
-      } catch (err) {
-        return done(err);
-      }
-    });
   });
 });
 
